bc7ee7533ba96d2ec7e398feda67d208ea876c2c
pathas <pathas@naver.com>
2020-05-27 12:44:11 +0900

    Use Query using useQuery from @apollo/react-hooks, gql from apollo-boost

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,23 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  query {
+    movies(limit: 20, rating: 9) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1 key={movie.id}>{movie.id}</h1>);
+  }
+  console.log(loading, error);
+};
