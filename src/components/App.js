4101030673a134173baf04912e3904d4a6e0cc7a
pathas <pathas@naver.com>
2020-05-27 11:12:03 +0900

    reset css, router setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:id" component={Detail} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
